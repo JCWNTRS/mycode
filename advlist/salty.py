2ad88d72d008acb20e56d1ee01623c2861fefcd5
jw261@ntrs.com <student@bchd.9daf6403-6d51-4025-802a-b337cb5515d7>
2021-10-25 15:33:56 +0000

    About complex lists

---
+++ advlist/salty.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+icecream= ["flavors", "salty"]
+northerntrust= ["Bala", "Inderjeet", "Jon", "Nikita", "Steve", "Vinod"]
+typeoicecream = (99)
+icecream.append (typeoicecream)
+name = input("pick a number between 0 & 5: ")
+num= int(name)
+print(num)
+print(f"{icecream[-1]} {icecream[0]} and {northerntrust[num]} chooses to be {icecream[1]}.")
+#print(icecream[2] + " and " + northerntrust[num] + " chooses to be " + icecream[1] )
