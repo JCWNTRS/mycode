89be6bfaade9463933d8168e5bf4f1f2cd7c65a1
jw261@ntrs.com <student@bchd.9daf6403-6d51-4025-802a-b337cb5515d7>
2021-10-25 12:26:39 +0000

    learning about input function

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect IP string input from the user
+    user_input = input("Please enter an IPv4 IP address:")
+     
+    ## print IP address
+    print("You told me the IPv4 address is: ", user_input)
+
+    ## Collect vendor string input from the user
+    Vendor_String = input("Please the vendor name: ")
+
+    ## print Vendor name
+    print("You told me the vendor name is:", Vendor_String)
+main()
