227b724918a1f35e0a6f503b120d9fd4f2c7e29b
jw261@ntrs.com <student@bchd.9daf6403-6d51-4025-802a-b337cb5515d7>
2021-10-28 09:09:48 +0000

    Day 4 challenge

---
+++ challenge20211028/challenge.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+classinfo = {"all": 
+    [
+        {
+            "First": "Bala",
+            "Last": "Kambale",
+            "Skill Level": "G.O.A.T.",
+            "Spirit Animal": "zebra",
+            "Super Power": "Mind control",
+        },
+        {
+            "First": "Inderjeet",
+            "Last": "Khalsa",
+            "Skill Level": "astonishing",
+            "Spirit Animal": "seal",
+            "Super Power": "Technopathy",
+        },
+        {
+            "First": "Jon",
+            "Last": "Whiteside",
+            "Skill Level": "stunning",
+            "Spirit Animal": "ram",
+            "Super Power": "Levitation",
+        },
+        {
+            "First": "Nikita",
+            "Last": "Nikale",
+            "Skill Level": "wondrous",
+            "Spirit Animal": "otter",
+            "Super Power": "Psychic",
+        },
+        {
+            "First": "Steve",
+            "Last": "Spike",
+            "Skill Level": "breathtaking",
+            "Spirit Animal": "lion",
+            "Super Power": "Portal creation",
+        },
+        {
+            "First": "Vinod",
+            "Last": "Singh",
+            "Skill Level": "wonderful",
+            "Spirit Animal": "badger",
+            "Super Power": "Immortal",
+        },
+    ]
+}
+
+for x in classinfo["all"]:
+    #if "Jon" == x["First"]:
+     print(x["First"])
