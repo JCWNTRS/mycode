5a391570ffa025b6ad3ceca649a22c5385d4a9fa
jw261@ntrs.com <student@bchd.9daf6403-6d51-4025-802a-b337cb5515d7>
2021-10-28 15:08:06 +0000

    Pulling data from an API - part 2

--- myapi/myapi.py
+++ myapi/myapi.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
+
+#Import required modules
 import requests as rq
-
+#Set variables
 URL= "http://www.boredapi.com/api/activity/"
-
-howmany = input("How many activities would you like me to suggest?")
-
-num = int(howmany)
-
-
+#Get number of required activities from the user
+num = int(input("How many activities would you like me to suggest?"))
+#Confirm number back to user
 print(f"Getting you {num} random activities")
-
-print(num)
+#Run loop and grab 'num' + 1 activities, write to screen and write to file.
 for n in range(1, num + 1):
     activity = rq.get(URL).json()
     print(activity["activity"])
     with open('/home/student/mycode/myapi/activities.txt','a') as file:
         file.write(activity["activity"] + "\n")
-
+#Tell user the activities have been written to a file. 
 print("I've also written them to activities.txt, so you can look at them later")
